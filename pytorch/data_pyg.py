8331fd6036ecfe85b350dbf63bacb70a0cc4ec5e
mahdiphl <mahdi.pahlavani78@gmail.com>
2026-08-11 12:24:04 +0000

    Changin data source

---
+++ pytorch/data_pyg.py
@@ -0,0 +1,46 @@
+import torch
+from torch.utils.data import Dataset
+from torch_geometric.datasets import ModelNet
+from torch_geometric.transforms import Compose, SamplePoints, NormalizeScale
+
+class TranslatePointCloud:
+    def __call__(self, data):
+        xyz1 = torch.empty(3).uniform_(2./3., 3./2.)
+        xyz2 = torch.empty(3).uniform_(-0.2, 0.2)
+        data.pos = data.pos * xyz1 + xyz2
+        return data
+
+class ShufflePoints:
+    def __call__(self, data):
+        perm = torch.randperm(data.pos.size(0))
+        data.pos = data.pos[perm]
+        return data
+
+train_transform = Compose([
+    SamplePoints(num=1024),   # set from args.num_points
+    NormalizeScale(),
+    TranslatePointCloud(),
+    ShufflePoints(),
+])
+
+test_transform = Compose([
+    SamplePoints(num=1024),
+    NormalizeScale(),
+])
+
+class ModelNet40PyG(Dataset):
+    """Wraps torch_geometric ModelNet, returns (pointcloud, label) like the old ModelNet40."""
+    def __init__(self, num_points, partition='train'):
+        transform = train_transform if partition == 'train' else test_transform
+        self.dataset = ModelNet(root='data/ModelNet40', name='40',
+                                 train=(partition == 'train'), transform=transform)
+        self.num_points = num_points
+
+    def __getitem__(self, item):
+        data = self.dataset[item]
+        pointcloud = data.pos[:self.num_points]        # (N, 3) float32
+        label = data.y                                  # scalar tensor
+        return pointcloud, label
+
+    def __len__(self):
+        return len(self.dataset)
--- pytorch/main.py
+++ pytorch/main.py
@@ -16,7 +16,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.optim.lr_scheduler import CosineAnnealingLR
-from data import ModelNet40
+#from data import ModelNet40
+from data_py import ModelNet40PyG
 from model import PointNet, DGCNN
 import numpy as np
 from torch.utils.data import DataLoader
@@ -37,9 +38,9 @@ def _init_():
     os.system('cp data.py checkpoints' + '/' + args.exp_name + '/' + 'data.py.backup')
 
 def train(args, io):
-    train_loader = DataLoader(ModelNet40(partition='train', num_points=args.num_points), num_workers=8,
+    train_loader = DataLoader(ModelNet40PyG(partition='train', num_points=args.num_points), num_workers=8,
                               batch_size=args.batch_size, shuffle=True, drop_last=True)
-    test_loader = DataLoader(ModelNet40(partition='test', num_points=args.num_points), num_workers=8,
+    test_loader = DataLoader(ModelNet40PyG(partition='test', num_points=args.num_points), num_workers=8,
                              batch_size=args.test_batch_size, shuffle=True, drop_last=False)
 
     device = torch.device("cuda" if args.cuda else "cpu")
@@ -136,7 +137,7 @@ def train(args, io):
 
 
 def test(args, io):
-    test_loader = DataLoader(ModelNet40(partition='test', num_points=args.num_points),
+    test_loader = DataLoader(ModelNet40PyG(partition='test', num_points=args.num_points),
                              batch_size=args.test_batch_size, shuffle=True, drop_last=False)
 
     device = torch.device("cuda" if args.cuda else "cpu")
